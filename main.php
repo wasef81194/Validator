bc8ba1b1c64249f01ff4b8b6497ab65d411a671e
Alexandra Wasef <alex.wasef@gmail.com>
2021-04-24 20:55:22 +0200

    Instrusion de la page test dans le main, la page main permtet pour l'instant de corriger toute les pages e css liés au lien entrés. ERrur quans je fais sa avec le html je l'es don mis en commentaire pour l'instant.

--- main.php
+++ main.php
@@ -6,85 +6,101 @@
 	include_once 'typeerror.php';
 	include_once 'url.php';
 	include_once 'warningcss.php';
+	include_once 'test.php';
+
+
+	
+	$url = 'http://wasef.alwaysdata.net/';
+	$Taburl = getTabUrl($url);
+	//var_dump($Taburl);
+	for ($x=0; $x < CountTabUrl($url) ; $x++) { 
+		$urls = $Taburl[$x];
+		//echo $urls;
+
+		$URLS = New URLS($urls);
+		
 
 	//---------------------------------------------------------------------------------------------------CSS--------------------------------------------------------------------------------------------
+		echo "<h2><b>La page à corriger : </b>". $URLS->GetURL()."</h2>";
 
-	$URLS = New URLS('http://mygame.alwaysdata.net');
-	
-	echo "<h2><b>La page à corriger : </b>". $URLS->GetURL()."</h2>";
+		$APICSS = $URLS->GetURLValidatorCSS();
+		$recup_data = file_get_contents($APICSS);
+		$data = json_decode($recup_data,TRUE);
+		//var_dump($data);
+		$errorcount = $data["cssvalidation"]["result"]["errorcount"];
+		$warningcount = $data["cssvalidation"]["result"]["warningcount"];
+		//echo $errorcount;
+		for ($i=0; $i <$errorcount; $i++) {
+			$line = $data["cssvalidation"]["errors"][$i]['line'];
+			$context = $data["cssvalidation"]["errors"][$i]['context'];
+			$type = $data["cssvalidation"]["errors"][$i]['type'];
+			$message = $data["cssvalidation"]["errors"][$i]['message'];
 
-	$url = $URLS->GetURLValidatorCSS();
-	$recup_data = file_get_contents($url);
-	$data = json_decode($recup_data,TRUE);
-	//var_dump($data);
-	$errorcount = $data["cssvalidation"]["result"]["errorcount"];
-	$warningcount = $data["cssvalidation"]["result"]["warningcount"];
+			$ErrorCSS = New ErrorCSS($line,$context,$type,$message);
 
-	for ($i=0; $i <$errorcount; $i++) {
-		$line = $data["cssvalidation"]["errors"][$i]['line'];
-		$context = $data["cssvalidation"]["errors"][$i]['context'];
-		$type = $data["cssvalidation"]["errors"][$i]['type'];
-		$message = $data["cssvalidation"]["errors"][$i]['message'];
+		
+			echo $ErrorCSS;
 
-		$ErrorCSS = New ErrorCSS($line,$context,$type,$message);
+		}
+		for ($i=0; $i <$warningcount; $i++) {
 
-	
-		echo $ErrorCSS;
+			$line = $data["cssvalidation"]["warnings"][$i]['line'];
+			$type = $data["cssvalidation"]["warnings"][$i]['type'];
+			$message = $data["cssvalidation"]["warnings"][$i]['message'];
+			$WarningCSS = New WarningCSS($line,$type,$message);
+		
+			echo $WarningCSS;
+		}
+
+
 
 	}
-	for ($i=0; $i <$warningcount; $i++) {
-
-		$line = $data["cssvalidation"]["warnings"][$i]['line'];
-		$type = $data["cssvalidation"]["warnings"][$i]['type'];
-		$message = $data["cssvalidation"]["warnings"][$i]['message'];
-		$WarningCSS = New WarningCSS($line,$type,$message);
-	
-		echo $WarningCSS;
-	}
-
-
-
-//-------------------------------------------------------------------------------------------------------HTML--------------------------------------------------------------------------------------------------
-   //Recupere l'API  du site W3C en JSON  // j'ai mis mon site http://yourgame.alwaysdata.net-
 
 	
 
-	$url = $URLS->GetURLValidatorHTML();
+	/*	//-------------------------------------------------------------------------------------------------------HTML--------------------------------------------------------------------------------------------------
+	   	//Recupere l'API  du site W3C en JSON  // j'ai mis mon site http://yourgame.alwaysdata.net-
 
-	$options = array(
-	  'http'=>array(
-	    'method'=>"GET",
-	    'header'=>"Accept-language: en\r\n" .
-	              "Cookie: foo=bar\r\n" .  // check function.stream-context-create on php.net
-	              "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36\r\n" // i.e. An iPad 
-	  )
-	);
-	// reprise du code de l'ancien projet permettant de retrouerner l'url s'éléctionner JSON en Tableau
-	$context = stream_context_create($options);
-	$recup_data = file_get_contents($url, false, $context);
+		
 
-	$data = json_decode($recup_data ,true);// Récupre les donnnées json en php
+		$url = $URLS->GetURLValidatorHTML();
 
-	$number = count($data["messages"]);
+		$options = array(
+		  'http'=>array(
+		    'method'=>"GET",
+		    'header'=>"Accept-language: en\r\n" .
+		              "Cookie: foo=bar\r\n" .  // check function.stream-context-create on php.net
+		              "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36\r\n" // i.e. An iPad 
+		  )
+		);
+		// reprise du code de l'ancien projet permettant de retrouerner l'url s'éléctionner JSON en Tableau
+		$context = stream_context_create($options);
+		$recup_data = file_get_contents($url, false, $context);
+
+		$data = json_decode($recup_data ,true);// Récupre les donnnées json en php
+
+		$number = count($data["messages"]);
 
 
-	// Affiche les erreur html de la page  http://yourgame.alwaysdata.net
+		// Affiche les erreur html de la page  http://yourgame.alwaysdata.net
 
 
-	for ($i=0; $i <$number ; $i++) {
-		$type = $data["messages"][$i]['type'];
-		$message = $data["messages"][$i]['message'];
-		$extract = $data["messages"][$i]['extract'];
-		$lastLine = $data["messages"][$i]['lastLine'];
-		$firstLine = $data["messages"][$i]['firstLine'];
-		$lastColumn =  $data["messages"][$i]['lastColumn'];
-		$firstColumn = $data["messages"][$i]['firstColumn'];
+		for ($f=0; $f <$number ; $f++) {
+			$type = $data["messages"][$f]['type'];
+			$message = $data["messages"][$f]['message'];
+			$extract = $data["messages"][$f]['extract'];
+			$lastLine = $data["messages"][$f]['lastLine'];
+			$firstLine = $data["messages"][$f]['firstLine'];
+			$lastColumn =  $data["messages"][$f]['lastColumn'];
+			$firstColumn = $data["messages"][$f]['firstColumn'];
 
-		$ErrorHTML = New ErrorHTML($type,$message,$extract,$lastLine,$firstLine,$lastColumn,$firstColumn);
+			$ErrorHTML = New ErrorHTML($type,$message,$extract,$lastLine,$firstLine,$lastColumn,$firstColumn);
 
-	
-		echo $ErrorHTML;
+		
+			echo $ErrorHTML;
 
-	}
+		}
+
+*/
 
  ?>
--- test.php
+++ test.php
@@ -1,14 +1,51 @@
 <?php
-$url = 'http://mygame.alwaysdata.net';
+/*$url = 'https://www.google.com/';
 $pattern = '#(?:src|href|path|xmlns(?::xsl)?)\s*=\s*(?:"|\')\s*(.+)?\s*(?:"|\')#Ui';
 $subject = file_get_contents($url);
 preg_match_all($pattern, $subject, $matches, PREG_PATTERN_ORDER);
 
 $nombre = count($matches[1]);
-echo $url."/";
+$taburl = [];
+
 foreach($matches[1] as $match)
 {
-	echo $match.'</br>';
+	$urls = $url;
+	$urls .= $match;
+	array_push($taburl,$urls);
+	$urls ='';
 }
+echo $urls;
+$number = count($taburl);
+echo $number;
+*/
+function getTabUrl($url){
+	$taburl = [];
+	$pattern = '#(?:href|path|xmlns(?::xsl)?)\s*=\s*(?:"|\')\s*(.+)?\s*(?:"|\')#Ui';
+	$subject = file_get_contents($url);
+	preg_match_all($pattern, $subject, $matches, PREG_PATTERN_ORDER);
+
+	$nombre = count($matches[1]);
+	array_push($taburl,$url);//ajoute l'url sélctionner dans le tableau
+
+	foreach($matches[1] as $match)
+	{
+		$urls = $url;
+		$urls .= $match;
+		array_push($taburl,$urls); //ajoute les urls rejoint sur la page 
+		$urls ='';
+	}
+	return $taburl;
+}
+//var_dump(getTabUrl('http://wasef.alwaysdata.net/'));
+
+function CountTabUrl($url){
+	$taburl = getTabUrl($url);
+	$count = count($taburl);
+	return $count;
+}
+//echo CountTabUrl('http://wasef.alwaysdata.net/');
+
+//$url = getTabUrl('http://wasef.alwaysdata.net/');
+//echo $url[0];
 
 ?>
